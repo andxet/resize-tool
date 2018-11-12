a549e7b30b321ce505f7ae4cb9883250d242b727
Andrea Peretti <and.xet@gmail.com>
2018-11-12 23:24:01 +0100

    Created demo scene added event system
    Added highlight on handles

---
+++ Assets/Scripts/HighlightOnHover.cs
@@ -0,0 +1,46 @@
+﻿#define DEBUG
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HighlightOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
+{
+    public Color hoverColor = Color.red;
+    Material mMaterial;
+    Color mOriginalColor;
+
+    //////////////////////////////////////////////////////////
+    void Start () 
+    {
+        Renderer rend = GetComponent<Renderer>();
+        if(rend != null)
+            mMaterial = rend.material;
+#if DEBUG //Let's assume that when the release is built, theese checks are passed
+        if (rend == null || mMaterial == null)
+        {
+        Debug.LogError("HighlightOnHover " + name + ": component non correctly initialized.");
+            enabled = false;
+            return;
+        }
+#endif
+        mOriginalColor = mMaterial.color;
+	}
+
+    //////////////////////////////////////////////////////////
+	void Update () 
+    {
+		
+	}
+
+    //////////////////////////////////////////////////////////
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mMaterial.color = hoverColor;
+    }
+
+    //////////////////////////////////////////////////////////
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mMaterial.color = mOriginalColor;
+    }
+
+}
