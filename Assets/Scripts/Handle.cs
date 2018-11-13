2d3f3057647e983b6df4f8201fb1c9004ad5d8a5
Andrea Peretti <peretti.and@gmail.com>
2018-11-13 22:55:59 +0100

    WIP handle

--- Assets/Scripts/Handle.cs
+++ Assets/Scripts/Handle.cs
@@ -2,56 +2,48 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Handle : MonoBehaviour, IDragHandler, IEndDragHandler 
+public class Handle : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    Mesh mMesh;
-    Plane mPlane;
+   Mesh mMesh;
+   Plane mPlane;
+   public Resizer objectToResize;
 
-    //////////////////////////////////////////////////////////
-    void Start () 
-    {
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
-        if(meshFilter != null)
-            mMesh = meshFilter.mesh;
+   //////////////////////////////////////////////////////////
+   void Start()
+   {
+      MeshFilter meshFilter = GetComponent<MeshFilter>();
+      if (meshFilter != null)
+         mMesh = meshFilter.mesh;
 #if DEBUG //Let's assume that when the release is built, theese checks are passed
-        if (meshFilter == null || mMesh == null)
-        {
-        Debug.LogError("Handle " + name + ": component non correctly initialized.");
-            enabled = false;
-            return;
-        }
+      if (meshFilter == null || mMesh == null)
+      {
+         Debug.LogError("Handle " + name + ": component non correctly initialized.");
+         enabled = false;
+         return;
+      }
 #endif
-        mPlane = new Plane(Vector3.up, transform.position.y);
-	}
+      mPlane = new Plane(Vector3.up, transform.position.y);
+   }
 
-    //////////////////////////////////////////////////////////
-    void Update()
-    {
+   //////////////////////////////////////////////////////////
+   public void OnBeginDrag(PointerEventData eventData)
+   {
+      mPlane = new Plane(Vector3.up, transform.position);
+   }
 
-    }
+   //////////////////////////////////////////////////////////
+   public void OnDrag(PointerEventData eventData)
+   {
+      Camera cam = eventData.pressEventCamera;
+      Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+      float distance;
+      mPlane.Raycast(ray, out distance);
+      transform.position = ray.GetPoint(distance);
+      objectToResize.Resize(transform.position);
+   }
 
-    //////////////////////////////////////////////////////////
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("DRAG");
-        Camera cam = eventData.pressEventCamera;
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        //mPlane.distance = transform.position.y;
-        float distance;
-        mPlane.Raycast(ray, out distance);
-        Debug.Log(ray.GetPoint(distance));
-        transform.position = ray.GetPoint(distance);
-    }
-
-    //////////////////////////////////////////////////////////
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("END DRAG");
-    }
-
-    //////////////////////////////////////////////////////////
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(mPlane.normal * mPlane.distance, mPlane.normal * mPlane.distance + mPlane.normal);
-    }
+   //////////////////////////////////////////////////////////
+   public void OnEndDrag(PointerEventData eventData)
+   {
+   }
 }
