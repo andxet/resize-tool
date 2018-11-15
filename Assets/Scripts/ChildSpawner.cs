b695283bb3710dd575a063deb008780680c00697
Andrea Peretti <and.xet@gmail.com>
2018-11-15 21:39:39 +0100

    Finished child spawner

--- Assets/Scripts/ChildSpawner.cs
+++ Assets/Scripts/ChildSpawner.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ChildSpawner : MonoBehaviour
 {
+   enum Edge
+   {
+      NORTH,
+      SOUTH,
+      EAST,
+      WEST
+   }
+
    public GameObject objectToMonitor;
    public GameObject objectToPlace;
 
@@ -12,18 +20,22 @@ public class ChildSpawner : MonoBehaviour
    [Tooltip("Space between elements")]
    public float padding = 0.20f;
 
-   Renderer mRenderer;
-   float mChildWidth;
 
-   List<GameObject> horizontalObjects = new List<GameObject>();
-   List<GameObject> verticalObjects = new List<GameObject>();
+   Renderer mFollowingRenderer;
+   float mChildWidth;
+   Bounds mLastBounds;
+
+
+   Dictionary<Edge, List<GameObject>> mChilds = new Dictionary<Edge, List<GameObject>>();
+   //List<GameObject> horizontalObjects = new List<GameObject>();
+   //List<GameObject> verticalObjects = new List<GameObject>();
 
    //////////////////////////////////////////////////////////
    void Start()
    {
       Renderer[] rends = {};
       if (objectToMonitor)
-         mRenderer = objectToMonitor.GetComponent<Renderer>();
+         mFollowingRenderer = objectToMonitor.GetComponent<Renderer>();
       if (objectToPlace)
       {
          
@@ -40,30 +52,47 @@ public class ChildSpawner : MonoBehaviour
       }
 
 #if DEBUG //Let's assume that when the release is built, theese checks are passed
-      if (objectToMonitor == null || mRenderer == null || objectToPlace == null || rends.Length == 0)
+      if (objectToMonitor == null || mFollowingRenderer == null || objectToPlace == null || rends.Length == 0)
       {
          Debug.LogError("ChildSpawner " + name + ": component non correctly initialized.");
          enabled = false;
          return;
       }
-#endif
+      #endif
+      mChilds.Add(Edge.NORTH, new List<GameObject>());
+      mChilds.Add(Edge.SOUTH, new List<GameObject>());
+      mChilds.Add(Edge.WEST,  new List<GameObject>());
+      mChilds.Add(Edge.EAST,  new List<GameObject>());
    }
 
    //////////////////////////////////////////////////////////
    void Update()
    {
-      Bounds bounds = mRenderer.bounds;
-      //Rect rect = mRenderer.bounds;
+      Bounds bounds = mFollowingRenderer.bounds;
 
+      //Counterclockwise
+      Vector3 startPoint = bounds.center;
+      if (Mathf.Abs(bounds.size.x - mLastBounds.size.x) > 0.01f || Mathf.Abs(bounds.size.z - mLastBounds.size.z) > 0.01f)
+      {
+         FitChilds(mChilds[Edge.NORTH], bounds.size.x, Vector3.left, new Vector3(startPoint.x + bounds.extents.x, 0, startPoint.z + bounds.extents.z));
+         FitChilds(mChilds[Edge.SOUTH], bounds.size.x, Vector3.right, new Vector3(startPoint.x - bounds.extents.x, 0, startPoint.z - bounds.extents.z));
+         FitChilds(mChilds[Edge.WEST], bounds.size.z, Vector3.back, new Vector3(startPoint.x - bounds.extents.x, 0, startPoint.z + bounds.extents.z));
+         FitChilds(mChilds[Edge.EAST], bounds.size.z, Vector3.forward, new Vector3(startPoint.x + bounds.extents.x, 0, startPoint.z - bounds.extents.z));
+         mLastBounds = bounds;
+      }
+   }
 
+   //////////////////////////////////////////////////////////
+   void FitChilds(List<GameObject> currentList, float length, Vector3 direction, Vector3 startPoint)
+   {
       //Available width
-      float width = bounds.size.x - 2 * margin;
+      float width = length - 2 * margin;
       if (width > 0)
       {
-         
-         float chairWithPadding = mChildWidth + padding;
-         int numChairs = (int)(width / chairWithPadding);
-         float remain = width - numChairs * chairWithPadding;
+         //Number of child
+         float childWithPadding = mChildWidth + padding;
+         int numChairs = (int)(width / childWithPadding);
+         float remain = width - numChairs * childWithPadding;
          if (remain > mChildWidth)
          {
             numChairs++;
@@ -72,30 +101,32 @@ public class ChildSpawner : MonoBehaviour
          else
             remain += padding;
 
-         //Be sure that the right amount of child are present
-         int childDifference = numChairs - horizontalObjects.Count;
-         if(childDifference < 0)
+         //Create and delete gameobjects to match the number
+         int childDifference = numChairs - currentList.Count;
+         if (childDifference < 0)
          {
-            List<GameObject> objectToDelete = horizontalObjects.GetRange(numChairs, Mathf.Abs(childDifference));
+            List<GameObject> objectToDelete = currentList.GetRange(numChairs, Mathf.Abs(childDifference));
             foreach (GameObject go in objectToDelete)
                Destroy(go);
-            horizontalObjects.RemoveRange(numChairs, Mathf.Abs(childDifference));
+            currentList.RemoveRange(numChairs, Mathf.Abs(childDifference));
          }
-         else if(childDifference > 0)
+         else if (childDifference > 0)
          {
             for (int i = 0; i < childDifference; i++)
-               horizontalObjects.Add(Instantiate(objectToPlace));
+               currentList.Add(Instantiate(objectToPlace));
          }
 
+         //Place all the objects
+         Quaternion rotation = Quaternion.FromToRotation(Vector3.right, direction);
 
-         Vector3 startingPoint = bounds.center;
-         startingPoint.x += -bounds.extents.x + margin + remain/2 + mChildWidth/2;
-         startingPoint.y = 0;
-         startingPoint.z += -bounds.extents.z;
-         foreach(GameObject go in horizontalObjects)
+         startPoint += direction * (margin + remain / 2 + mChildWidth / 2);
+         foreach (GameObject go in currentList)
          {
-            go.transform.position = startingPoint;
-            startingPoint.x += mChildWidth + padding;
+            go.transform.position = startPoint;
+            startPoint += direction * (mChildWidth + padding);
+
+            //Rotate the object
+            go.transform.localRotation = rotation;
          }
       }
    }
